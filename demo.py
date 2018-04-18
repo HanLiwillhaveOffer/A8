9a365bf21c3d14ce5d5999b38fb7a1fd96e838cb
pyscraper <hanl4@illinois.edu>
2018-04-17 21:52:03 -0500

    a demo that predicts the winning rate of all the players over 10000 simulations.

--- demo.py
+++ demo.py
@@ -1,25 +1,33 @@
 from random import choice, randint
 import random
-def _3pointer_contest(_3PG):
-    """
-    simulate the score based on the shooting percetage of the player
-    :param _3PG: shooting percentage of a player
-    :return: the simulated score of the player
-    """
-    score=0
-    for i in range(25):
-        shoot = random.randint(1,sum(_3PG))
-        if i<20:
-            if shoot<=_3PG[0]:
-                score+=1
+
+class player():
+    name=''
+    _3PG = 0
+    def __init__(self,name,_3PG):
+        self.name = name
+        self._3PG=_3PG
+
+    def _3pointer_contest(self):
+        """
+        simulate the score based on the shooting percetage of the player
+        :param _3PG: shooting percentage of a player
+        :return: the simulated score of the player
+        """
+        score=0
+        for i in range(25):
+            shoot = random.randint(1,100)
+            if i<20:
+                if shoot<=self._3PG:
+                    score+=1
+                else:
+                    pass
             else:
-                pass
-        else:
-            if shoot<=_3PG[0]:
-                score+=2
-            else:
-                pass
-    return score
+                if shoot<=self._3PG:
+                    score+=2
+                else:
+                    pass
+        return score
 
 def sort_dic(dic):
     """
@@ -42,15 +50,15 @@ def get_game_result(candidate_number,player_list):
     next_round_candidate={}
     next_round_candidate_list = []
     for player in player_list:
-        score = _3pointer_contest([player[1], player[2]])
-        result[player[0]] = score
+        score = player._3pointer_contest()
+        result[player.name] = score
         #print('{} got {} this round!'.format(player[0], score))
     threshold = sort_dic(result)[candidate_number-1][1]
     for key, value in result.items():
         if value >= threshold:
             next_round_candidate[key] = value
     for player in player_list:
-        if player[0] in next_round_candidate.keys():
+        if player.name in next_round_candidate.keys():
             next_round_candidate_list.append(player)
     return next_round_candidate_list
 
@@ -59,12 +67,13 @@ def one_simulation():
     Simulate one game
     :return: the winner of the game
     """
-    curry=['curry',70,30]
-    george=['george',65,35]
-    beal = ['beal',68,32]
-    thompson=['thompson',72,28]
-    gordan = ['gordan',69,31]
-    player_list = [curry,george,beal,thompson,gordan]
+    curry = player('curry', 70)
+    george =player('george', 65)
+    beal = player('beal',68)
+    thompson = player('thompson',72)
+    gorden = player('gorden',69)
+    booker = player('booker',74)
+    player_list = [curry,george,beal,thompson,gorden,booker]
     over_time_flag=True
     candidate_list = get_game_result(3,player_list)
     #print('The players in the final game are:')
@@ -79,7 +88,7 @@ def one_simulation():
             candidate_list=get_game_result(1,candidate_list)
 
 
-    winner = candidate_list[0][0]
+    winner = candidate_list[0].name
     #print('The winner is {}!'.format(candidate_list[0][0]))
     return winner
 
@@ -92,7 +101,8 @@ def main():
     print('The winning rate of george is {}.'.format(winner_list.count('george') / len(winner_list)))
     print('The winning rate of beal is {}.'.format(winner_list.count('beal') / len(winner_list)))
     print('The winning rate of thompson is {}.'.format(winner_list.count('thompson') / len(winner_list)))
-    print('The winning rate of gordan is {}.'.format(winner_list.count('gordan') / len(winner_list)))
+    print('The winning rate of gorden is {}.'.format(winner_list.count('gorden') / len(winner_list)))
+    print('The winning rate of booker is {}.'.format(winner_list.count('booker') / len(winner_list)))
 if __name__ == '__main__':
     main()
 
