8ffb2397c7d556028d892f2374c180ce427a7635
pyscraper <hanl4@illinois.edu>
2018-04-24 20:46:29 -0500

    added choose_strategy function

--- demo.py
+++ demo.py
@@ -37,13 +37,13 @@ class player():
             if lefttime <= 0:
                 break
             elif i in range(self.bonus*5-4 ,self.bonus*5+1):
-                if shoot<=self._3PG*(1-self.stamina/100*(i))*state*math.log2(0.5*min(shootingtime,2)+1):
+                if shoot<=self._3PG*(1-self.stamina/100*(i))*state*math.log2(0.5*shootingtime+1):
                     score+=2
                     offset+=2
                 else:
                     offset-=3
             else:
-                if shoot<=self._3PG*(1-self.stamina/100*(i))*state*math.log2(0.5*min(shootingtime,2)+1):
+                if shoot<=self._3PG*(1-self.stamina/100*(i))*state*math.log2(0.5*shootingtime+1):
                     score+=1
                     offset+=2
                 else:
@@ -54,6 +54,11 @@ class player():
 
 
     def get_on_fire_state(self,offset):
+        """
+        define the state of a player for every shoot.
+        :param offset: the offset adding to the basic probability to turn on-fire state
+        :return: state: whether the state is on-fire of not
+        """
         prob = random.randint(1,100)
         if prob<(self.on_fire_prob+offset):
             state = self.growing
@@ -73,6 +78,34 @@ class player():
         else:
             runtime = 0
         return runtime
+
+    def choose_strategy(self):
+        """
+        let the player simulate every possible strategy and bonus point to find out the best combination
+        """
+        best_bonus = 0
+        best_strategy = 0
+        best_score = 0
+        for bonus in range(1,6):
+            self.bonus = bonus
+            for strategy in range(1,8):
+                avg_score = 0
+                score_list = []
+                self.strategy=strategy
+                for round in range(1000):
+                    score = self._3pointer_contest()
+                    score_list.append(score)
+                avg_score = sum(score_list)/len(score_list)
+                #print('The average score with bonus {} and strategy {} is: {} '.format(self.bonus,self.strategy,avg_score))
+                if avg_score>best_score:
+                    best_score = avg_score
+                    best_bonus = self.bonus
+                    best_strategy=self.strategy
+        self.bonus = best_bonus
+        self.strategy=best_strategy
+        print('The final stratgy this player applied is: bonus {} and strategy {} with the average score of {}'.format(self.bonus,self.strategy,best_score))
+
+
 
     def shootingtime(self, i, lefttime):
         """
@@ -122,7 +155,7 @@ def sort_dic(dic):
     """
     return sorted(dic.items(),key = lambda item:item[1],reverse=True)
 
-def get_game_result(candidate_number,player_list,simulation_index):
+def get_game_result(candidate_number,player_list):
     """
     To get the player list of next round based on the number of required candidate and the player list of current round.
     :param candidate_number: the number of required candidate
@@ -137,11 +170,7 @@ def get_game_result(candidate_number,player_list,simulation_index):
     for player in player_list:
         score = player._3pointer_contest()
         result[player.name] = score
-        print('{} got {} this round!'.format(player.name, score))
-        if simulation_index ==1:
-            stat[player.name] = [score]
-        else:
-            stat[player.name].append(score)
+        #print('{} got {} this round!'.format(player.name, score))
 
     #print(result)
     #print(stat)
@@ -154,23 +183,16 @@ def get_game_result(candidate_number,player_list,simulation_index):
             next_round_candidate_list.append(player)
     return next_round_candidate_list
 
-def one_simulation(simulation_index):
+def one_simulation(player_list):
     """
     Simulate one game
     :return: the winner of the game
     """
-    curry = player('curry', 90,1,15,1.28)
-    george =player('george', 65,1,25,1.38)
-    beal = player('beal',68,1,22,1.32)
-    thompson = player('thompson',50,0.5,10,1.25)
-    gorden = player('gorden',69,1.5,13,1.3)
-    booker = player('booker',74,1.5,18,1.21)
 
-    player_list = [curry,george,beal,thompson,gorden,booker]
     over_time_flag=True
-    candidate_list = get_game_result(3,player_list,simulation_index)
+    candidate_list = get_game_result(3,player_list)
     #print('The players in the final game are:')
-    candidate_list = get_game_result(1, candidate_list,simulation_index+1)
+    candidate_list = get_game_result(1, candidate_list)
     while over_time_flag:
         if len(candidate_list)==1:
             over_time_flag=False
@@ -178,38 +200,44 @@ def one_simulation(simulation_index):
             #print('The players in the overtime game are:')
             #for player in candidate_list:
                 #print(player[0])
-            candidate_list=get_game_result(1,candidate_list,simulation_index+1)
+            candidate_list=get_game_result(1,candidate_list)
 
     winner = candidate_list[0].name
     #print('The winner is {}!'.format(candidate_list[0][0]))
-    return winner,player_list
+    return winner,candidate_list
 
 
 def main():
+    curry = player('curry', 90,1,15,1.28)
+    george =player('george', 65,1,25,1.38)
+    beal = player('beal',68,1,22,1.32)
+    thompson = player('thompson',50,0.5,10,1.25)
+    gorden = player('gorden',69,1.5,13,1.3)
+    booker = player('booker',74,1.5,18,1.21)
+    player_list = [curry,george,beal,thompson,gorden,booker]
     winner_list = []
-    player_list = []
-    for simulation_index in range(100):
-        winner, player_list = one_simulation(simulation_index + 1)
+    for simulation_index in range(10000):
+        winner, player_list = one_simulation(player_list)
         winner_list.append(winner)
-    print('--------------------------')
-    print("Average score")
-    print('{0:10} {1:<5}'.format(player_list[0].name, sum(stat[player_list[0].name]) / len(stat[player_list[0].name])))
-    print('{0:10} {1:<5}'.format(player_list[1].name, sum(stat[player_list[1].name]) / len(stat[player_list[1].name])))
-    print('{0:10} {1:<5}'.format(player_list[2].name, sum(stat[player_list[2].name]) / len(stat[player_list[2].name])))
-    print('{0:10} {1:<5}'.format(player_list[3].name, sum(stat[player_list[3].name]) / len(stat[player_list[3].name])))
-    print('{0:10} {1:<5}'.format(player_list[4].name, sum(stat[player_list[4].name]) / len(stat[player_list[4].name])))
-    print('{0:10} {1:<5}'.format(player_list[5].name, sum(stat[player_list[5].name]) / len(stat[player_list[5].name])))
     print('--------------------------')
     print("Ｗinning rate")             
     print('{0:11} {1:<5}'.format(player_list[0].name, winner_list.count(player_list[0].name) / len(winner_list)))
-    print('{0:11} {1:<5}'.format(player_list[1].name, winner_list.count(player_list[0].name) / len(winner_list)))
-    print('{0:11} {1:<5}'.format(player_list[2].name, winner_list.count(player_list[1].name) / len(winner_list)))
-    print('{0:11} {1:<5}'.format(player_list[3].name, winner_list.count(player_list[2].name) / len(winner_list)))
-    print('{0:11} {1:<5}'.format(player_list[4].name, winner_list.count(player_list[3].name) / len(winner_list)))
-    print('{0:11} {1:<5}'.format(player_list[5].name, winner_list.count(player_list[4].name) / len(winner_list)))
+    print('{0:11} {1:<5}'.format(player_list[1].name, winner_list.count(player_list[1].name) / len(winner_list)))
+    print('{0:11} {1:<5}'.format(player_list[2].name, winner_list.count(player_list[2].name) / len(winner_list)))
+    print('{0:11} {1:<5}'.format(player_list[3].name, winner_list.count(player_list[3].name) / len(winner_list)))
+    print('{0:11} {1:<5}'.format(player_list[4].name, winner_list.count(player_list[4].name) / len(winner_list)))
+    print('{0:11} {1:<5}'.format(player_list[5].name, winner_list.count(player_list[5].name) / len(winner_list)))
 
 if __name__ == '__main__':
-    main()
+    curry = player('curry', 90,1,15,1.28)
+    george =player('george', 65,1,25,1.38)
+    beal = player('beal',68,1,22,1.32)
+    thompson = player('thompson',50,0.5,10,1.25)
+    gorden = player('gorden',69,1.5,13,1.3)
+    booker = player('booker',74,1.5,18,1.21)
+    player_list = [curry, george, beal, thompson, gorden, booker]
+    for player in player_list:
+        player.choose_strategy()
 
 
 
